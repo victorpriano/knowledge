6d05ad732272a2c52717896fb60b00c5b0c5eb92
victor <victorcuru@gmail.com>
2022-06-21 11:12:11 -0300

    api de estatisticas

--- config/routes.js
+++ config/routes.js
@@ -46,4 +46,8 @@ module.exports = app => {
         .all(app.config.passport.authenticate())
         .get(app.api.article.getByCategory)
 
+    app.route('/stats')
+        .all(app.config.passport.authenticate())
+        .get(app.api.stat.get)
+
 }
